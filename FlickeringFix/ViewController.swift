6d18d9feebccf656c5021559ab51d5e5918b4fec
Abinab Dangi <abinabdangi@Abinabs-MacBook-Pro.local>
2022-05-20 10:32:13 +0545

    initial commit

---
+++ FlickeringFix/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  FlickeringFix
+//
+//  Created by Abinab Dangi on 20/05/2022.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
